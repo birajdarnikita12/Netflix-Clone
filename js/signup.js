04e439461e602d50189a27c43f89e920ff13b707
Nikita Birajdar <89728413+birajdarnikita12@users.noreply.github.com>
2023-03-10 23:24:28 +0530

    update

---
+++ js/signup.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+    $("#submit-btn").click(function () {
+        console.log("im in");
+        var str = $("#email-ip").val();
+        $("#email").text(str)
+   });
+
+
+
+// }
+// )
